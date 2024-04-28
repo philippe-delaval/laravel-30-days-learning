e2687d5a8efe3c7e1e789cea74afddd2d95a733b
philippe-delaval <klareg@gmail.com>
2024-04-28 18:17:05 +0200

    feat: Refactor job listing and update packages
    Updated job listing view for enhanced readability by removing unnecessary condition checks. Migration to Vite from Tailwind CSS was done in layout blade, and corresponding changes in package.json and package-lock.json. Updated job title CSS for better visibility. Additional package dependencies were also added.

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -5,16 +5,11 @@
 
     <div class="space-y-4">
         @foreach($jobs as $job)
-            <a href="/jobs/{{ $job['id'] }}"
-               class="block px-4 py-6 border border-gray-200 rounded-lg">
-                @if($job->employer)
-                    <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
-                @else
-                    <div class="font-bold text-blue-500 text-sm">Employer Unknown</div>
-                @endif
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
 
                 <div>
-                    <strong>{{ $job['title'] }}</strong> Pays {{ $job['salary'] }} per year.
+                    <strong class="text-laracasts">{{ $job['title'] }}</strong> Pays {{ $job['salary'] }} per year.
                 </div>
             </a>
         @endforeach
